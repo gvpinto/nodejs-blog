dcf5a3bc93792898491f19f5dff687b6fdbbc6d2
Glenn Pinto <gvpinto@gmail.com>
2023-04-25 08:15:34 -0400

    Complete Lesson 48

--- blog-nodejs/comments/index.js
+++ blog-nodejs/comments/index.js
@@ -16,24 +16,44 @@ app.get('/posts/:id/comments', (req, res) => {
 });
 
 app.post('/posts/:id/comments', async (req, res) => {
+
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+
+    comments.push({ id: commentId, content, status: "pending" });
+
     commentsByPostId[req.params.id] = comments;
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
             content,
+            status: "pending",
             postId: req.params.id
         }
     });
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log(`Comments: Event received: ${req.body.type}`);
+    const { type, data } = req.body;
+    if (type === 'CommentModerated') {
+        const { id, content, status, postId } = data;
+        const comment = commentsByPostId[postId];
+        comment.status = status;
+
+        await axios.post('http://piercingstripes-linux:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                content,
+                status
+            }
+        });
+    }
     res.send({});
 });
 
