76e5b68b3b12f7770c426d9fd604979956f00948
Glenn Pinto <gvpinto@gmail.com>
2023-04-29 16:42:06 -0400

    complete pod creating without deployment

--- blog-nodejs/query/index.js
+++ blog-nodejs/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 
 const app = express();
@@ -13,9 +14,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = {
@@ -36,10 +35,23 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+
+};
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
     // console.log(posts);
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log(`Query listening on port 4002`);
+    const res = await axios.get('http://piercingstripes-linux:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event: ', event.type);
+        handleEvent(event.type, event.data);
+    }
 });
